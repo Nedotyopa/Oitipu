8f1aa1479091c622e2827b31bf230e95f81b3d41
olgasn <olgasn@inbox.ru>
2016-07-16 00:16:41 +0300

    Инициализация

--- ToplivoCodeFirst/Models/ToplivoDbInitializer.cs
+++ ToplivoCodeFirst/Models/ToplivoDbInitializer.cs
@@ -7,23 +7,47 @@ namespace ToplivoCodeFirst.Models
     {
         protected override void Seed(ToplivoContext db)
         {
+            int tanks_number = 100;
+            int fuels_number = 10;
+            int operations_number = 10;
+
             //Заполнение таблицы емкостей
-            for (int tankID=1; tankID < 101; tankID++)
+            string[] tank_voc = { "Цистерна_", "Ведро_", "Бак_", "Фляга_" };
+            string[] material_voc = { "Сталь", "Дерево", "Алюминий", "Полиэтилен" };
+            int count_tank_voc = tank_voc.GetLength(0);
+            int count_material_voc = material_voc.GetLength(0);
+            
+            for (int tankID=1; tankID <= tanks_number; tankID++)
             {
-                string tankType = "Емкость_" + tankID.ToString();
-                string tankMaterial = "Материал_" + tankID.ToString();
                 Random randObj = new Random(tankID);
+                string tankType = tank_voc[randObj.Next(count_tank_voc)] + tankID.ToString();
+                string tankMaterial = material_voc[randObj.Next(count_material_voc)];
                 float tankWeight = 500*(float)randObj.NextDouble();
                 float tankVolume = 200 * (float)randObj.NextDouble();
                 db.Tanks.Add(new Tank { TankID = tankID, TankType = tankType, TankWeight = tankWeight, TankVolume = tankVolume, TankMaterial = tankMaterial });
             }
+
             //Заполнение таблицы видов топлива
-            for (int fuelID = 1; fuelID < 101; fuelID++)
+            string[] fuel_voc = { "Нефть_", "Бензин_", "Керосин_", "Мазут_" };
+            int count_fuel_voc = fuel_voc.GetLength(0);
+            for (int fuelID = 1; fuelID <= fuels_number; fuelID++)
             {
-                string fuelType = "Топливо_" + fuelID.ToString();
                 Random randObj = new Random(fuelID);
+                string fuelType = fuel_voc[randObj.Next(count_fuel_voc)] + fuelID.ToString();
                 float fuelDensity = 2 * (float)randObj.NextDouble();
                 db.Fuels.Add(new Fuel { FuelID = fuelID, FuelType = fuelType, FuelDensity = fuelDensity });
+            }
+
+            //Заполнение таблицы 
+
+            for (int operationID = 1; operationID <= operations_number; operationID++)
+            {
+                Random randObj = new Random(operationID);
+                int tankID = randObj.Next(tanks_number);
+                int fuelID= randObj.Next(fuels_number);
+                DateTime today = DateTime.Now;
+                DateTime operationdate = today.AddDays(-operationID);
+                db.Operations.Add(new Operation { OperationID= operationID, TankID = tankID, FuelID = fuelID, Inc_Exp=10, Date= operationdate });
             }
 
             db.SaveChanges();
