7803e7e00f0ecb531a81307186daaaf24455ba3c
Oleg <olgasn@inbox.ru>
2016-07-17 13:47:17 +0300

    Добавлен еще один инициализатор

---
+++ ToplivoCodeFirst/Models/ToplivoDbInitializer2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace ToplivoCodeFirst.Models
+{
+    public class ToplivoDbInitializer2 : DropCreateDatabaseAlways<ToplivoContext>
+    {
+        protected override void Seed(ToplivoContext db)
+        {
+            string readPath = @"FillDB.sql";
+
+            string SQLstring = "";
+            try
+            {
+                using (StreamReader sr = new StreamReader(readPath, System.Text.Encoding.Default))
+                {
+                    SQLstring = sr.ReadToEnd();
+                }
+                
+
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
