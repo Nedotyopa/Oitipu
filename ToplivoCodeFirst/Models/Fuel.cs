635a707a2d0e1c7b4b414bfcf40aa80258d9ba4c
Olgasn <olgasn@inbox.ru>
2016-07-27 08:54:53 +0300

    Добавлены аннотации к  свойствам моделей

--- ToplivoCodeFirst/Models/Fuel.cs
+++ ToplivoCodeFirst/Models/Fuel.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ToplivoCodeFirst.Models
 {
     public class Fuel
     {
         //Id Топлива
+        [Key]
+        [Display(Name = "Код топлива")]
         public int FuelID { get; set; }
-        //Название вида топлива
+        //Наименование вида топлива
+        [Display(Name = "Наименование топлива")]
         public string FuelType { get; set; }
         //Плотность вида топлива
+        [Display(Name = "Плотность топлива")]
         public float FuelDensity { get; set; }
         public virtual ICollection<Operation> Operations { get; set; }
  
