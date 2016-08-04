28a22350b9fcce6ff472247ae342b798a459bd49
Olgasn <olgasn@inbox.ru>
2016-08-04 11:20:45 +0300

    Добавлена возможность отображения и изменения данных Fuels в табличном виде с использование JS элемента JQGrid

---
+++ ToplivoCodeFirst/Controllers/JQGridFuelsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using ToplivoCodeFirst.Models;
+using System.Data.Entity;
+
+namespace ToplivoCodeFirst.Controllers
+{
+    public class JQGridFuelsController : Controller
+    {
+        // GET: JQGridFuels
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public JsonResult GetFuels(string sidx, string sort, int page, int rows)
+        {
+            ToplivoContext db = new ToplivoContext();
+            sort = (sort == null) ? "" : sort;
+            int pageIndex = Convert.ToInt32(page) - 1;
+            int pageSize = rows;
+            var FuelList = db.Fuels.Select(
+                    t => new
+                    {
+                        t.FuelID,
+                        t.FuelType,
+                        t.FuelDensity
+                    });
+            int totalRecords = FuelList.Count();
+            var totalPages = (int)Math.Ceiling((float)totalRecords / (float)rows);
+            if (sort.ToUpper() == "DESC")
+            {
+                FuelList = FuelList.OrderByDescending(t => t.FuelType);
+                FuelList = FuelList.Skip(pageIndex * pageSize).Take(pageSize);
+            }
+            else
+            {
+                FuelList = FuelList.OrderBy(t => t.FuelType);
+                FuelList = FuelList.Skip(pageIndex * pageSize).Take(pageSize);
+            }
+            var jsonData = new
+            {
+                total = totalPages,
+                page,
+                records = totalRecords,
+                rows = FuelList
+            };
+            return Json(jsonData, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public string Create([Bind(Exclude = "FuelID")] Fuel Model)
+        {
+            ToplivoContext db = new ToplivoContext();
+            string msg;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Fuels.Add(Model);
+                    db.SaveChanges();
+                    msg = "Запись сохранена";
+                }
+                else
+                {
+                    msg = "Модель не прошла валидацию";
+                }
+            }
+            catch (Exception ex)
+            {
+                msg = "Произошла ошибка:" + ex.Message;
+            }
+            return msg;
+        }
+        public string Edit(Fuel Model)
+        {
+            ToplivoContext db = new ToplivoContext();
+            string msg;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(Model).State = EntityState.Modified;
+                    db.SaveChanges();
+                    msg = "Запись сохранена";
+                }
+                else
+                {
+                    msg = "Модель не прошла валидацию";
+                }
+            }
+            catch (Exception ex)
+            {
+                msg = "Error occured:" + ex.Message;
+            }
+            return msg;
+        }
+        public string Delete(string id)
+        {
+            ToplivoContext db = new ToplivoContext();
+            Fuel fuel = db.Fuels.Find(Convert.ToInt32(id));
+            db.Fuels.Remove(fuel);
+            db.SaveChanges();
+            return "Запись удалена";
+        }
+
+    }
+}
---
+++ ToplivoCodeFirst/Scripts/scriptFuels.js
@@ -0,0 +1,79 @@
+﻿$(function () {
+    $("#jqGrid").jqGrid({
+        url: "/JQGridFuels/GetFuels",
+        datatype: 'json',
+        mtype: 'Get',
+        colNames: ['FuelID', 'Вид топлива', 'Плотность'],
+        colModel: [
+            { key: true, hidden: true, name: 'FuelID', index: 'FuelID', editable: true },
+            { key: false, name: 'FuelType', index: 'FuelType', sortable: true, editable: true },
+            { key: false, name: 'FuelDensity', index: 'FuelDensity', editable: true }],
+        pager: jQuery('#jqControls'),
+        rowNum: 10,
+        rowList: [10, 20, 30, 40, 50],
+        sortname: "FuelType",
+        sortorder: "DESC", // порядок сортировки,
+        height: '100%',
+        viewrecords: true,
+        caption: 'Виды топлива',
+        emptyrecords: 'Нет видов топлива для отображения',
+        jsonReader: {
+            root: "rows",
+            page: "page",
+            total: "total",
+            records: "records",
+            repeatitems: false,
+            Id: "0"
+        },
+        autowidth: true,
+        multiselect: false
+    }).navGrid('#jqControls',
+    {
+        edit: true,
+        edittext: "Редактировать",
+        add: true,
+        addtext: "Добавить",
+        del: true,
+        deltext: "Удалить",
+        search: true,
+        searchtext: "Найти",
+        refresh: true,
+        refreshtext: "Обновить"
+    },
+        {
+            zIndex: 100,
+            url: '/JQGridFuels/Edit',
+            closeOnEscape: true,
+            closeAfterEdit: true,
+            recreateForm: true,
+            afterComplete: function (response) {
+                if (response.responseText) {
+                    alert(response.responseText);
+                }
+            }
+        },
+        {
+            zIndex: 100,
+            url: "/JQGridFuels/Create",
+            closeOnEscape: true,
+            closeAfterAdd: true,
+            afterComplete: function (response) {
+                if (response.responseText) {
+                    alert(response.responseText);
+                }
+            }
+        },
+        {
+            zIndex: 100,
+            url: "/JQGridFuels/Delete",
+            closeOnEscape: true,
+            closeAfterDelete: true,
+            recreateForm: true,
+            msg: "Вы уверены, что хотите удалить запись? ",
+            afterComplete: function (response) {
+                if (response.responseText) {
+                    alert(response.responseText);
+                }
+            }
+        });
+});
