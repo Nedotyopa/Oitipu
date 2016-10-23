0f64b3ca7ea32e5186a819bf0b5b217cfd97d7f8
Oleg <olgasn@inbox.ru>
2016-10-23 20:19:31 +0300

    Добавлен проект для тестирования приложения

---
+++ ToplivoCodeFirst.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToplivoCodeFirst.Controllers;
+using System.Web.Mvc;
+
+namespace ToplivoCodeFirst.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Топливная база", result.ViewBag.Message);
+        }
+
+        
+    }
+}
--- ToplivoCodeFirst/Controllers/FuelsController.cs
+++ ToplivoCodeFirst/Controllers/FuelsController.cs
@@ -1,6 +1,4 @@
-﻿using System.Data.Entity;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using ToplivoCodeFirst.Models;
